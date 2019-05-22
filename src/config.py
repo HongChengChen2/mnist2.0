b5d9df637cbbe1c8de0e9f7b29b049e8ca72851a
洪成晨 <hongchengchen@hongchengchendeMacBook-Pro.local>
2019-05-22 10:41:54 +0800

    config change

--- src/config.py
+++ src/config.py
@@ -317,7 +317,8 @@ if __C.VERSION is None:
   __C.VERSION = _auto_version(__C)
 
 # Source data directory path
-__C.SOURCE_DATA_PATH = '../data/source_data'
+# __C.SOURCE_DATA_PATH = '../data/source_data'
+__C.SOURCE_DATA_PATH = '/home/leander/Leander/capsule-tf/data/'
 
 # Preprocessed data path
 __C.DPP_DATA_PATH = '../data/preprocessed_data'

e0f68b4274150db55f2c1edf25c7ca899d41b342
洪成晨 <hongchengchen@hongchengchendeMacBook-Pro.local>
2019-06-13 21:02:20 +0800

    change config for tesra

--- src/config.py
+++ src/config.py
@@ -317,7 +317,7 @@ if __C.VERSION is None:
   __C.VERSION = _auto_version(__C)
 
 # Source data directory path
- __C.SOURCE_DATA_PATH = '../data/source_data'
+__C.SOURCE_DATA_PATH = '../data/source_data'
 #__C.SOURCE_DATA_PATH = '/home/leander/Leander/capsule-tf/data/source_data'
 
 # Preprocessed data path
